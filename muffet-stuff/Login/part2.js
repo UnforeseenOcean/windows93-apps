202f55005cf0d16400a51fa97c7b047f43d0ef40
PeanutFIsh <peanutfish2@gmail.com>
2017-07-28 13:09:02 +0200

    Removed Muffet Stuff and updated Index.

--- muffet-stuff/Login/part2.js
+++
@@ -1,8 +0,0 @@
-function takeControl() {
-document.write("<body bgcolor='000000'>");
-document.write("<script src='part1.js'></script>");
-document.write("<h1 style='color:white'>AHUHUHUHU!!! Don't guess who am i...</h1>");
-document.write('<button onClick="chooseFight()">Fight!</button>');
-document.write('<button onClick="chooseGuess()">Guess who is this person. Do not be genocide</button>');
-document.write("</body>");
-};
