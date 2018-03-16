a3bc54add3d8669fa5498ee7dd256857a99ab903
MICROWAVE <peanutfish2@gmail.com>
2018-03-16 10:12:51 +0100

    Update engine.js

--- mcs/engine.js
+++ mcs/engine.js
@@ -1,1 +1,8 @@
+// 3pC Computer simulation engine
+// by MICROWAVE. Version 1.0
 
+function startengine() {
+  document.write("<h1>Engine</h1>");
+  aA = prompt("Byte 1 - Digit 1");
+  aB = prompt("Byte 1 - Digit 2");
+}
